5b4e9a2c99bd826509e9d0e844e72f8b62bc5507
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-05-01 15:41:28 +0330

    Added Book Service close #4

--- src/main/java/com/pureamorous/spring_rbac_jwt/controller/BookController.java
+++ src/main/java/com/pureamorous/spring_rbac_jwt/controller/BookController.java
@@ -1,14 +1,19 @@
 package com.pureamorous.spring_rbac_jwt.controller;
 
 
+import com.pureamorous.spring_rbac_jwt.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
     @RequestMapping("get")
 
     public String getService(){
-        return "My service";
+        return bookService.getService();
     }
 }
---
+++ src/main/java/com/pureamorous/spring_rbac_jwt/service/BookService.java
@@ -0,0 +1,11 @@
+package com.pureamorous.spring_rbac_jwt.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    public String getService(){
+        return "Book service";
+    }
+}
