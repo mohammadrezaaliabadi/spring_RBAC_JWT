d431ddc05e5035a553fb3907967ff6404e577680
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-05-05 12:57:58 +0330

    Added Security filter chain for spring security close #15

--- src/main/java/com/pureamorous/spring_rbac_jwt/configuration/SecurityConfig.java
+++ src/main/java/com/pureamorous/spring_rbac_jwt/configuration/SecurityConfig.java
@@ -5,12 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(matcherRegistry->{
+            matcherRegistry.requestMatchers("api/auth").permitAll();
+            matcherRegistry.anyRequest().authenticated();
+        });
+        return http.build();
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
