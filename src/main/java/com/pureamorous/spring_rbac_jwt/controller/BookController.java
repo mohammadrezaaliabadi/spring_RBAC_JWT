45d9243097ca2f6af65b004b2fa1d8508f7fbd5b
Mohammad Reza AliAbadi <44574954+mohammadrezaaliabadi@users.noreply.github.com>
2024-05-05 18:46:26 +0330

    Adding authorization in spring security. close #19

--- src/main/java/com/pureamorous/spring_rbac_jwt/controller/BookController.java
+++ src/main/java/com/pureamorous/spring_rbac_jwt/controller/BookController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,29 +18,33 @@ public class BookController {
     @Autowired
     private BookService service;
 
-
+    @PreAuthorize("hasAuthority('select')")
     @GetMapping
     public List<Book> getBooks(){
         return service.findAll();
     }
 
+    @PreAuthorize("hasAuthority('select')")
     @GetMapping("/{id}")
     public Book find(@PathVariable Integer id){
         return service.findById(id);
     }
+
+    @PreAuthorize("hasAuthority('insert')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void create(@Valid @RequestBody Book book){
         service.save(book);
     }
 
+    @PreAuthorize("hasAuthority('remove')")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remove(@PathVariable Integer id) {
         service.remove(id);
     }
 
-
+    @PreAuthorize("hasAuthority('update')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@Valid @RequestBody Book book) {
